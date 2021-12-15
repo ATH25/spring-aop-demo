aa49c7811f067f06f2a1d7c9dccb70350d50948e
ATH25 <aju_thomas@yahoo.com>
2021-12-14 23:06:06 -0500

    add explanation to the point cut

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/UseAccessAspect.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/UseAccessAspect.java
@@ -18,7 +18,7 @@ public class UseAccessAspect {
 	//What kind of method calls I would intercept
 	//execution(* PACKAGE.*.*(..))
 	
-	@Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+	@Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")//in the package specified, any class, any method with any kind/number of arguments. 
 	public void before(JoinPoint joinPoint){
 		logger.info(" Check for user access ");
 		logger.info(" Allowed execution for {}", joinPoint);
