af8961c8761b93c0bfeb5736750774a794ae0c92
ATH25 <aju_thomas@yahoo.com>
2022-01-23 21:02:31 -0500

    first commit from the windows 11 laptop

--- src/main/java/com/in28minutes/spring/aop/springaop/aspect/MethodExecutionCalculationAspect.java
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/MethodExecutionCalculationAspect.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-@Aspect
+@Aspect 
 @Configuration
 public class MethodExecutionCalculationAspect {
 
